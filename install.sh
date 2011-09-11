125fe1ec177b83857c03348a041090c4952fad12
Ichabond <apollo@baconseed.org>
2011-09-11 18:18:10 +0200

    Use images.baconbits.org instead of imgur

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 cp BeautifulSoup.py /usr/local/bin/
-cp MultiPostHandler.py /usr/local/bin/
+cp MultipartPostHandler.py /usr/local/bin/
 cp microdata.py /usr/local/bin
 cp pythonbits.py /usr/local/bin/pythonbits
 chmod o+x /usr/local/bin/pythonbits
