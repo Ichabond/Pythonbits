e0f9745d145ef97957f97f6c23a1fba2424a8530
Ichabond <apollo@baconseed.org>
2014-08-25 13:36:45 +0200

    Qscale is unneeded
    qscale is only relevant for streams, not for images. Removed from ffmpeg options.

--- Ffmpeg.py
+++ Ffmpeg.py
@@ -52,7 +52,7 @@ class FFMpeg (object):
 			for stop in stops:
 				imgs.append(self.tempdir+"screen%s.png" % stop)
 				subprocess.Popen([r"ffmpeg","-ss",str((self.duration * stop)/100), "-i", self.file, "-vframes", "1", 
-									"-y", "-qscale", "0",  "-f", "image2", imgs[-1] ], 
+									"-y",  "-f", "image2", imgs[-1] ], 
 									stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()
 		except OSError:
 			sys.stderr.write("Error: Ffmpeg not installed, refer to http://www.ffmpeg.org/download.html for installation")
